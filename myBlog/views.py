5c48e7e4d80b9bd6552e63df4a29d19e767989f0
RUCHI <ruchi21writes@gmail.com>
2017-12-13 12:24:38 +0530

    Added views to create/edit blog post inside the site.

--- myBlog/views.py
+++ myBlog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -9,5 +11,32 @@ def post_list(request):
     return render(request, 'myBlog/post_list.html',{'posts': posts})
 
 def post_detail(request, pk):
-    posts = get_object_or_404(post, pk=pk)
+    post = get_object_or_404(post, pk=pk)
     return render(request, 'myBlog/post_detail.html', {'posts': post}) #bug hai idhar ek abhi
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'myBlog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'myBlog/post_edit.html', {'form': form})
